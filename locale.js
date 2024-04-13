f0e90295296734442fb0ea6128097cfadbee8776
satchmo <135016180+corecolorjp@users.noreply.github.com>
2024-04-13 09:24:16 +0900

    remove dummy locale

--- locale.js
+++ locale.js
@@ -554,10 +554,3 @@ const AG_GRID_LOCALE_JA = {
     timeFormatHHMMSS: 'HH:MM:SS',
     timeFormatHHMMSSAmPm: 'HH:MM:SS AM/PM',
 }
-
-const AG_GRID_LOCALE_ZZZ = {};
-// Create a dummy locale based on english but prefix everything with zzz
-Object.keys(AG_GRID_LOCALE_EN).forEach(function(key) {
-    if (key === 'thousandSeparator' || key === 'decimalSeparator') { return; }
-    AG_GRID_LOCALE_ZZZ[key] = 'zzz-' + AG_GRID_LOCALE_EN[key];
-});
